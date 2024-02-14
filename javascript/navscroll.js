dad50c853fae160e5bb21c1cb4da4680027e3cf7
Hugo <43926158+unchihugo@users.noreply.github.com>
2024-02-14 18:52:35 +0100

    Add files via upload

---
+++ javascript/navscroll.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    var nav = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        nav.classList.add('nav-border');
+    } else {
+        nav.classList.remove('nav-border');
+    }
+});
