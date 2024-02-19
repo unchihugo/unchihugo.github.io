332ee966a6a045f0ce451a46f40241cb316c888e
Hugo <hu8go8li@gmail.com>
2024-02-19 21:57:22 +0000

    added page transitions with fun messages, added favicon, added icons

--- javascript/card-collage-hover.js
+++ javascript/card-collage-hover.js
@@ -1,5 +1,6 @@
 // change z-index of card-collage on hover to 1000
 const cardCollages = document.querySelectorAll('.card-collage');
+const cardContainer = document.querySelectorAll('.card-collage-container');
 
 cardCollages.forEach(collage => {
   const img = collage.querySelector('img');
--- javascript/loader.js
+++ javascript/loader.js
@@ -1,50 +1,66 @@
-document.querySelectorAll('a').forEach(function(a) {
-    a.addEventListener('click', function(e) {
+// loading messages
+const loadingMessages = [
+    'preparing the page for you <3',
+    'putting everything in its place',
+    'moving things around',
+    'getting things ready',
+    'just a moment!',
+    'collecting content for you <3',
+];
+
+
+// show loader when any link is clicked, excluding mailto, # and index#contact-me
+document.querySelectorAll('a').forEach(function (a) {
+    a.addEventListener('click', function (e) {
         if (this.getAttribute('href').startsWith('mailto')) return;
         if (this.getAttribute('href').startsWith('#')) return;
-        if (this.getAttribute('href').match('index#contact-me')) return;
         e.preventDefault();
 
         const destination = this.getAttribute('href');
 
         const loader = document.querySelector('#page-loader');
-        loader.style.top = (window.scrollY + window.innerHeight) + 'px';
+        loader.style.top = window.innerHeight + 'px';
         loader.style.display = 'block';
-        document.body.style.overflowY = 'hidden';
-        loader.style.top = window.scrollY + 'px';
+        setTimeout(function () {
+            loader.style.top = 0; // slide the loader up into view
+        }, 10);
 
-        setTimeout(function() {
-            window.location.href = destination;
+        setTimeout(function () {
+            window.location.href = destination; // go to destination after 250ms
         }, 250);
     });
 });
 
-window.addEventListener('load', function() {
+// show loader when new page is loaded or refreshed
+window.addEventListener('load', function () {
     const loader = document.querySelector('#page-loader');
     const content = document.querySelector('#page-loader-content');
-    if (this.window.location.href.match('index#contact-me')) {
-        document.body.style.overflowY = 'visible';
-        loader.style.display = 'none';
-    }
-    loader.style.top = window.scrollY + 'px';
+    const text = document.querySelector('#page-loader-content p');
+
+    loader.style.top = 0;
 
     var random = Math.floor(Math.random() * 10);
     if (random < 5) random = -(random + 3);
     else random += 3;
 
+    // select random loading message
+    const randomIndex = Math.floor(Math.random() * loadingMessages.length);
+    const randomMessage = loadingMessages[randomIndex];
+
+    text.innerHTML = randomMessage;
+
     content.style.transform = `scale(0) rotate(${-random}deg)`;
     content.style.display = 'block';
-    setTimeout(function() {
+    setTimeout(function () { // show content
         content.style.opacity = '1';
         content.style.transform = 'scale(1) rotate(0deg)';
-        setTimeout(function() {
+        setTimeout(function () { // hide content
             content.style.opacity = '0';
             content.style.transform = `scale(0) rotate(${random}deg)`;
-            setTimeout(function() {
-                loader.style.top = (window.scrollY + window.innerHeight) + 'px';
-                
-                setTimeout(function() {
-                    document.body.style.overflowY = 'visible';
+            setTimeout(function () { // slide loader out of view
+                loader.style.top = window.innerHeight + 'px';
+
+                setTimeout(function () {
                     loader.style.display = 'none';
                 }, 250);
             }, 150);
